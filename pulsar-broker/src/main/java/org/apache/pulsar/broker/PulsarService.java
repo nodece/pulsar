eca8e027b4cbd3cc89e08a78c85729562f42c200
Lari Hotari <lhotari@users.noreply.github.com>
2022-06-06 15:59:25 +0800

    Remove duplicate filter instances in Broker, Proxy and Function worker web server (#15637)
    - There were filter instances for each context path which made
      maxConcurrentHttpRequests and httpRequestsMaxPerSecond not work as expected.
    - Fixes the backpressure solution that is dependent on maxConcurrentHttpRequests and httpRequestsMaxPerSecond
      working properly
    - Fix invalid Jersey api usage
    
    (cherry picked from commit 94392a41fc8a6f7b431785bdc9701250b43c8464)
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -89,6 +89,7 @@ import org.apache.pulsar.broker.loadbalance.LoadManager;
 import org.apache.pulsar.broker.loadbalance.LoadReportUpdaterTask;
 import org.apache.pulsar.broker.loadbalance.LoadResourceQuotaUpdaterTask;
 import org.apache.pulsar.broker.loadbalance.LoadSheddingTask;
+import org.apache.pulsar.broker.lookup.v1.TopicLookup;
 import org.apache.pulsar.broker.namespace.NamespaceService;
 import org.apache.pulsar.broker.protocol.ProtocolHandlers;
 import org.apache.pulsar.broker.resourcegroup.ResourceGroupService;
@@ -96,6 +97,7 @@ import org.apache.pulsar.broker.resourcegroup.ResourceUsageTopicTransportManager
 import org.apache.pulsar.broker.resourcegroup.ResourceUsageTransportManager;
 import org.apache.pulsar.broker.resources.ClusterResources;
 import org.apache.pulsar.broker.resources.PulsarResources;
+import org.apache.pulsar.broker.rest.Topics;
 import org.apache.pulsar.broker.service.BrokerService;
 import org.apache.pulsar.broker.service.GracefulExecutorServicesShutdown;
 import org.apache.pulsar.broker.service.SystemTopicBaseTxnBufferSnapshotService;
@@ -868,21 +870,20 @@ public class PulsarService implements AutoCloseable, ShutdownService {
             // Ensure the VIP status is only visible when the broker is fully initialized
             return state == State.Started;
         });
+
         // Add admin rest resources
-        webService.addRestResources("/",
-                VipStatus.class.getPackage().getName(), false, vipAttributeMap);
-        webService.addRestResources("/",
-                "org.apache.pulsar.broker.web", false, attributeMap);
+        webService.addRestResources("/", false, vipAttributeMap, VipStatus.class.getPackage().getName());
         webService.addRestResources("/admin",
-                "org.apache.pulsar.broker.admin.v1", true, attributeMap);
+                true, attributeMap, "org.apache.pulsar.broker.admin.v1");
         webService.addRestResources("/admin/v2",
-                "org.apache.pulsar.broker.admin.v2", true, attributeMap);
+                true, attributeMap, "org.apache.pulsar.broker.admin.v2");
         webService.addRestResources("/admin/v3",
-                "org.apache.pulsar.broker.admin.v3", true, attributeMap);
-        webService.addRestResources("/lookup",
-                "org.apache.pulsar.broker.lookup", true, attributeMap);
-        webService.addRestResources("/topics",
-                            "org.apache.pulsar.broker.rest", true, attributeMap);
+                true, attributeMap, "org.apache.pulsar.broker.admin.v3");
+        webService.addRestResource("/lookup",
+                true, attributeMap, TopicLookup.class,
+                org.apache.pulsar.broker.lookup.v2.TopicLookup.class);
+        webService.addRestResource("/topics",
+                true, attributeMap, Topics.class);
 
         // Add metrics servlet
         webService.addServlet("/metrics",
