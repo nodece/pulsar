9e4328bea1bc546dad9d79d3d0dafb680a1f72d7
fengyubiao <yubiao.feng@streamnative.io>
2024-01-03 21:51:26 +0800

    [fix] [broker] Update topic policies as much as possible when some ex was thrown (#21810)
    After the topic policies update, there are many components will be updated one by one, even if the config of components has not been modified. There are the 11 components that need update:
    - `7` rate limiters(`publish`, `dispatch topic-level`, `dispatch subscription-level`,  `dispatch resourceGroup-level`, `subscribe API`, `replication`, `shadow topic replication`)
    - update ManagedLedger configs(`retention`, `offloader`)
    - start/stop replication
    - start/stop compaction
    - start/stop deduplication
    
    Once a component update fails, the following update will be skipped. It would cause a confusing thing: you want to set a retention policy, but it will be skipped due to the `update subscribe rate limiter` failure (you did not edit the `subscribe rate limitation policy`)
    
    Since none of the components in the above list have any additional dependencies for individual updates, ensuring success as much as possible is appropriate.
    
    - Update topic policies as much as possible even if some component updates fail, all component updates are still in the same thread, and they still update one by one, just throw the error later.
    - Rename `updatePublishDispatcher` to `updatePublishRateLimiter`
    
    (cherry picked from commit ed599673c7e60ab5bb02e1fb0615a7ff8e5d6430)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -2512,7 +2512,7 @@ public class BrokerService implements Closeable {
             forEachTopic(topic -> {
                 if (topic instanceof AbstractTopic) {
                     ((AbstractTopic) topic).updateBrokerPublishRate();
-                    ((AbstractTopic) topic).updatePublishDispatcher();
+                    ((AbstractTopic) topic).updatePublishRateLimiter();
                 }
             }));
     }
